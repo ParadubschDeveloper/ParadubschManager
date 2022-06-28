0b42ddcf774904650c7361c1eaee7147d19a5f6e
Crafter_Y <mrcrafter.yt1@gmail.com>
2022-06-28 15:12:08 +0200

    tab prefixes

--- src/main/java/de/paradubsch/paradubschmanager/lifecycle/TabDecorationManager.java
+++ src/main/java/de/paradubsch/paradubschmanager/lifecycle/TabDecorationManager.java
@@ -4,20 +4,29 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import de.paradubsch.paradubschmanager.ParadubschManager;
+import de.paradubsch.paradubschmanager.config.ConfigurationManager;
 import de.paradubsch.paradubschmanager.models.PlayerData;
 import de.paradubsch.paradubschmanager.util.Hibernate;
 import de.paradubsch.paradubschmanager.util.lang.Language;
 import de.paradubsch.paradubschmanager.util.lang.Message;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class TabDecorationManager implements Listener {
     private final JavaPlugin plugin;
@@ -26,11 +35,13 @@ public class TabDecorationManager implements Listener {
         this.plugin = plugin;
         Bukkit.getPluginManager().registerEvents(this, plugin);
         Bukkit.getScheduler().runTaskLater(plugin, this::broadcastTabDecorations, 20*5);
+        broadcastScoreboardTeams();
     }
 
     @EventHandler
     private void onPlayerJoin(PlayerJoinEvent event) {
         broadcastTabDecorations();
+        setScoreboardTeam(event.getPlayer());
     }
 
     @EventHandler
@@ -42,6 +53,14 @@ public class TabDecorationManager implements Listener {
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             for (Player p : Bukkit.getOnlinePlayers()) {
                 TabDecorationManager.displayTabDecorations(p);
+            }
+        });
+    }
+
+    private void broadcastScoreboardTeams() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                setScoreboardTeam(p);
             }
         });
     }
@@ -58,6 +77,70 @@ public class TabDecorationManager implements Listener {
             ParadubschManager.getInstance().getProtocolManager().sendServerPacket(p, packet);
         } catch (InvocationTargetException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void setScoreboardTeam(Player p) {
+        ConfigurationSection configurationSection = ConfigurationManager.getConfig().getConfigurationSection("tabprefix");
+        if (configurationSection == null) return;
+        Set<String> keys = configurationSection.getKeys(false);
+        String rankForPlayer = null;
+        List<String> ranks = new ArrayList<>();
+        keys.stream().sorted((str1, str2) -> {
+            String s1 = ConfigurationManager.getConfig().getString("tabprefix." + str1 + ".team");
+            String s2 = ConfigurationManager.getConfig().getString("tabprefix." + str2 + ".team");
+            if (s1 == null || s2 == null) {
+                return 0;
+            }
+            return s2.compareTo(s1);
+        }).forEach(ranks::add);
+        for (String rank : ranks) {
+            if (p.hasPermission("group." + rank)) rankForPlayer = rank;
+        }
+        applyScoreboard(p, rankForPlayer);
+    }
+
+    private static void applyScoreboard(Player p, String groupName) {
+        String team = ConfigurationManager.getConfig().getString("tabprefix." + groupName + ".team");
+        String prefix = ConfigurationManager.getConfig().getString("tabprefix." + groupName + ".prefix");
+        String color = ConfigurationManager.getConfig().getString("tabprefix." + groupName + ".color", "&7");
+        if (team == null || prefix == null) return;
+
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        Team scoreboardTeam = scoreboard.getTeam(team);
+        if (scoreboardTeam == null) {
+            scoreboardTeam = scoreboard.registerNewTeam(team);
+        }
+        scoreboardTeam.prefix(Component.text(ChatColor.translateAlternateColorCodes('&', prefix + " &7| ")));
+
+        color = color.replaceAll("&", "");
+        color = color.replaceAll("[^0-9a-f]", "");
+        ChatColor parsedChatColor = ChatColor.getByChar(color.charAt(0));
+        if (parsedChatColor == null) return;
+
+        scoreboardTeam.color(translateCharCode(parsedChatColor.getChar()));
+        scoreboardTeam.addEntry(p.getName());
+        p.setScoreboard(scoreboard);
+    }
+
+    public static NamedTextColor translateCharCode (char name) {
+        switch (name) {
+            case '0': return NamedTextColor.BLACK;
+            case '1': return NamedTextColor.DARK_BLUE;
+            case '2': return NamedTextColor.DARK_GREEN;
+            case '3': return NamedTextColor.DARK_AQUA;
+            case '4': return NamedTextColor.DARK_RED;
+            case '5': return NamedTextColor.DARK_PURPLE;
+            case '6': return NamedTextColor.GOLD;
+            case '8': return NamedTextColor.DARK_GRAY;
+            case '9': return NamedTextColor.BLUE;
+            case 'a': return NamedTextColor.GREEN;
+            case 'b': return NamedTextColor.AQUA;
+            case 'c': return NamedTextColor.RED;
+            case 'd': return NamedTextColor.LIGHT_PURPLE;
+            case 'e': return NamedTextColor.YELLOW;
+            case 'f': return NamedTextColor.WHITE;
+            default: return NamedTextColor.GRAY;
         }
     }
 }
